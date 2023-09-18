5a0ab656100b91be5f9ddfce9ae30000384a33c5
John Carges <johncarges@gmail.com>
2023-09-18 10:32:56 -0400

    adds patch and delete functionality

--- server/app.py
+++ server/app.py
@@ -47,6 +47,31 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        form = request.get_json()
+        for attr in form:
+            setattr(plant, attr, form[attr])
+        db.session.add(plant)
+        db.session.commit()
+
+        response = make_response(
+            plant.to_dict(),
+            200
+        )
+
+        return response
+    
+    def delete(self,id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        response = make_response(
+            '',
+            204
+        )
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
